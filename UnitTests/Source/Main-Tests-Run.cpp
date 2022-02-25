0b2719a94b881f9bdc17a8b9b811fcb869375c83
Janusz Wolak <januszvdm@gmail.com>
2022-02-25 17:21:40 +0100

    Main class TesnsionCpu and CmdArguments created

--- UnitTests/Source/Main-Tests-Run.cpp
+++ UnitTests/Source/Main-Tests-Run.cpp
@@ -3,12 +3,15 @@
 #include <gtest/gtest.h>
 #include "gmock/gmock.h"
 
+/*
 #include "EquinoxLoggerTests/File-Logger-Tests.cpp"
 #include "EquinoxLoggerTests/Console-Logger-Tests.cpp"
 #include "EquinoxLoggerTests/Logger-Level-Tests.cpp"
 #include "EquinoxLoggerTests/Logger-Output-Tests.cpp"
 #include "EquinoxLoggerTests/Logger-Log-Message-Macros-Tests.cpp"
 #include "EquinoxLoggerTests/Logger-Enable-Log-Levels-Macros-Tests.cpp"
+*/
+#include "TensionCpuTests/TensionCpuTests.cpp"
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
